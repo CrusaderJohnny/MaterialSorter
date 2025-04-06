0bcae44c591a6e2c19cb679374c212a5bcbe348f
CrusaderJohnny <17ark.leonardis@gmail.com>
2025-04-06 14:48:06 -0600

    updated main page and folders with router

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function App() {
+    return(
+        <View style={styles.container}>
+            <Text style={styles.text}>This is the first page</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1
+    },
+    text: {
+        textAlign: 'center'
+    }
+})
